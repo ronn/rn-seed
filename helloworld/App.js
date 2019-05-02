f6d4ee9c4ad48a52b6ef5cb94768a25058c7bb7e
ronn <ronnie-filth@hotmail.com>
2019-05-02 11:31:45 +0200

    HelloWorld

--- helloworld/App.js
+++ helloworld/App.js
@@ -21,9 +21,7 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Hello Guuuyss!!!</Text>
       </View>
     );
   }
@@ -40,10 +38,5 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
